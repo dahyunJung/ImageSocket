6b7d5a189edec81c87833736d0ed529e2365365c
dahyunJung <deh2246@naver.com>
2021-07-26 22:46:19 +0900

    약간 수정

--- cv_client2.py
+++ cv_client2.py
@@ -11,11 +11,12 @@ cam.set(4, 240)
 
 while True:
     ret, frame = cam.read()
-    result, frame = cv2.imencode('.jpg', frame)
-    data = np.array(frame)
-    stringData = data.tobytes()
- 
-    #서버에 데이터 전송(str(len(stringData))).encode().ljust(16)
-    s.sendall((str(len(stringData))).encode().ljust(16) + stringData)
+    result, frame = cv2.imencode('.jpg', frame) 
+    data = np.array(frame)  # type=numpy.ndarray print([[255][219][213]], ...)
+    stringData = data.tobytes()     # type=bytes
 
+    #서버에 데이터 전송(str(len(stringData))).encode().ljust(16)
+    #s.sendall((str(len(stringData))).encode().ljust(6) + stringData)
+    s.sendall(stringData)
+    
 cam.release()
--- cv_server2.py
+++ cv_server2.py
@@ -15,20 +15,20 @@ def recvall(sock, count):
  
 HOST=''     # 자신의 컴 ip
 PORT=8485
- 
+
 #TCP 사용
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((HOST,PORT))
-s.listen(10)
+s.listen()
 print('Socket now listening')
 
 #연결, conn에는 소켓 객체, addr은 소켓에 바인드 된 주소
-conn,addr=s.accept()
- 
+conn, addr=s.accept()
+
 while True:
 # client에서 받은 stringData의 크기 (==(str(len(stringData))).encode().ljust(16))
-    length = recvall(conn, 16)
-    stringData = recvall(conn, int(length))
+    length = recvall(conn, 6)       # buf 크기
+    stringData = recvall(conn, int(length))     # byte 데이터
     data = np.fromstring(stringData, dtype = 'uint8')    
     frame = cv2.imdecode(data, cv2.IMREAD_COLOR)    #data를 디코딩한다.
 
