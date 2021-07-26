d75a81bb423bd250ae360054d87b56ad9d877334
dahyunJung <deh2246@naver.com>
2021-07-27 04:24:27 +0900

    동영상test 수정

--- cv_client2.py
+++ cv_client2.py
@@ -9,14 +9,17 @@ cam = cv2.VideoCapture('1215.mp4')
 #cam.set(3, 320)
 #cam.set(4, 240)
 
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 10]
+
 while True:
     ret, frame = cam.read()
-    result, frame = cv2.imencode('.jpg', frame) 
+    result, frame = cv2.imencode('.jpg', frame, encode_param) 
     data = np.array(frame)  # type=numpy.ndarray print([[255][219][213]], ...)
     stringData = data.tobytes()     # type=bytes
 
     #서버에 데이터 전송(str(len(stringData))).encode().ljust(16)
     #s.sendall((str(len(stringData))).encode().ljust(6) + stringData)
     s.sendall(stringData)
-    
+    print(len(stringData))
+
 cam.release()
