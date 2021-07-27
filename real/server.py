1d6d7e56df00f908f4146d07acc3a5982b0c11c3
dahyunJung <deh2246@naver.com>
2021-07-28 00:41:25 +0900

    장소이동

--- real/server.py
+++ real/server.py
@@ -16,11 +16,17 @@ def recvall(sock, count):
 HOST=''     # 자신의 컴 ip
 PORT=8485
  
+#TCP 사용
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print('Socket created')
+ 
+#서버의 아이피와 포트번호 지정
 s.bind((HOST,PORT))
+print('Socket bind complete')
+# 클라이언트의 접속을 기다린다. (클라이언트 연결을 10개까지 받는다)
 s.listen(10)
 print('Socket now listening')
-
+ 
 #연결, conn에는 소켓 객체, addr은 소켓에 바인드 된 주소
 conn,addr=s.accept()
  
